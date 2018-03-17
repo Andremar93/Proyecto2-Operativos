abfd5fd44471a6a211a02ecf6d648397b48ed37c
Carlos Perez <capf96@gmail.com>
2018-03-17 16:11:28 -0400

    Algo de la funcion hash

--- proyecto2.c
+++ proyecto2.c
@@ -26,10 +26,44 @@ char archivo_indice[10000] = "indice.txt";
 int update = 1;
 int add = 1;
 
+typedef struct contenedor{
+    char palabra[1000];
+    char direccion[10000];
+}contenedor;
+
+
+contenedor * nuevo_contenedor(char* str){
+    contenedor *nuevo = (contenedor*) malloc(sizeof(contenedor));
+    strcpy(nuevo->palabra, str);
+    return nuevo;
+}
 /*----------------------------------------------------------------------------
                                 Funciones
 ----------------------------------------------------------------------------*/
 
+unsigned int hash(char* str) {
+    unsigned int length = strlen(str);
+    unsigned int hash = 5381;
+    unsigned int i = 0;
+
+    while(i < length){
+        hash = ((hash << 5) + hash) + (*str);
+        str++;
+        i++;
+    }
+
+    return hash % 10000;
+}
+
+void add_hash(contenedor* agregar, contenedor* indice[]){
+    unsigned int posicion = hash(agregar->palabra);
+    if(indice[posicion] == NULL){
+        indice[posicion] = agregar;
+    }
+    else{
+        printf("Que haremos cuando hay colisiones?\n");
+    }
+}
 
 /*----------------------------------------------------------------------------
                                     Main
@@ -37,6 +71,8 @@ int add = 1;
 
 int main(int argc, char *argv[]){
     getcwd(directorio_inicial, 10000);
+
+    contenedor *indice[10000];
 
     //Leemos todos los flags correspondientes y cambiamos las variables 
     //globales para manejarlos
@@ -70,6 +106,19 @@ int main(int argc, char *argv[]){
     printf("altura maxima: %d\n", altura_maxima);
     printf("Directorio inicial: %s\n", directorio_inicial);
     printf("Archivo indice: %s\n", archivo_indice);
+
+    
+    contenedor *prueba = nuevo_contenedor("archivo.txt");
+
+    add_hash(prueba, indice);
+    add_hash(prueba, indice);
+
+    prueba = nuevo_contenedor("quecosapasoaqui");
+
+    add_hash(prueba, indice);
+
+    printf("%s\n", indice[hash("archivo.txt")]->palabra);
+    printf("%s\n", indice[hash("quecosapasoaqui")]->palabra);
 
     return EXIT_SUCCESS;
 }
